091bd3e2f37f74f8737a9ef2d71b0583ed512e58
Ali Sindhi <64632604+MumtazAli123@users.noreply.github.com>
2024-03-31 09:09:03 +0500

    update

---
+++ android/app/src/main/kotlin/paysaw/com/vendor/MainActivity.kt
@@ -0,0 +1,5 @@
+package paysaw.com.vendor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,19 +1,23 @@
 import 'dart:async';
 
 import 'package:geocoding/geocoding.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:location/location.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
   final count = 0.obs;
 
+
+  var location  = "Location..".obs; // "Getting Location...
+
   var latitude = " Latitude..".obs; // "Getting Latitude...
   var longitude = " Longitude..".obs; // "Getting Longitude...
   var address = " Address..".obs; // "Getting Address...
   var cityName = "City Name..".obs; // "Getting City Name...
-  late StreamSubscription<Position> streamSubscription;
+  late StreamSubscription streamSubscription;
+
 
   @override
   void onInit() {
@@ -35,57 +39,66 @@ class HomeController extends GetxController {
   void increment() => count.value++;
 
   void getLocation() async {
-    bool serviceEnabled;
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
 
-    LocationPermission permission;
+    await Future.delayed(Duration(seconds: 2));
 
-       serviceEnabled = await Geolocator.isLocationServiceEnabled();
-       if (!serviceEnabled) {
-         await Geolocator.openLocationSettings();
-         return Future.error('Location services are disabled.');
-       }
-       permission = await Geolocator.checkPermission();
-       if (permission == LocationPermission.deniedForever) {
-         await Geolocator.openAppSettings();
-         return Future.error(
-             'Location permissions are permanently denied, we cannot request permissions.');
-       }
-       if (permission == LocationPermission.denied) {
-         permission = await Geolocator.requestPermission();
-         if (permission != LocationPermission.whileInUse &&
-             permission != LocationPermission.always) {
-           return Future.error(
-               'Location permissions are denied (actual value: $permission).');
-         }
-       }
-       streamSubscription = Geolocator.getPositionStream().listen((Position position) {
-         latitude.value = 'Latitude: ${position.latitude}';
-         longitude.value = 'Longitude: ${position.longitude}';
-         getAddressFromLatLng(position);
-          city(position);
-       });
 
-     }
 
-    Future <void> getAddressFromLatLng(Position position) async {
-       try {
-         // karachi
-          List<Placemark> placemarks = await placemarkFromCoordinates(24.8607, 67.0011);
-          Placemark place = placemarks[0];
-          address.value = 'Address: ${place.street}, ${place.locality}, ${place.administrativeArea}';
-         } catch (e) {
-         address.value = "Error: $e";
-       }
-    }
-    Future <void> city(Position position) async {
-        try {
-          // karachi
-          List<Placemark> placemarks = await placemarkFromCoordinates(24.8607, 67.0011);
-          Placemark place = placemarks[0];
-          cityName.value = 'City: ${place.locality}';
-        } catch (e) {
-          cityName.value = "Error: $e";
-        }
 
-    }
+  }
+
+
+
+    //    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    //    if (!serviceEnabled) {
+    //      await Geolocator.openLocationSettings();
+    //      return Future.error('Location services are disabled.');
+    //    }
+    //    permission = await Geolocator.checkPermission();
+    //    if (permission == LocationPermission.deniedForever) {
+    //      await Geolocator.openAppSettings();
+    //      return Future.error(
+    //          'Location permissions are permanently denied, we cannot request permissions.');
+    //    }
+    //    if (permission == LocationPermission.denied) {
+    //      permission = await Geolocator.requestPermission();
+    //      if (permission != LocationPermission.whileInUse &&
+    //          permission != LocationPermission.always) {
+    //        return Future.error(
+    //            'Location permissions are denied (actual value: $permission).');
+    //      }
+    //    }
+    //    streamSubscription = Geolocator.getPositionStream().listen((Position position) {
+    //      latitude.value = 'Latitude: ${position.latitude}';
+    //      longitude.value = 'Longitude: ${position.longitude}';
+    //      getAddressFromLatLng(position);
+    //       city(position);
+    //    });
+    //
+    //  }
+    //
+    // Future <void> getAddressFromLatLng(Position position) async {
+    //    try {
+    //      // karachi
+    //       List<Placemark> placemarks = await placemarkFromCoordinates(24.8607, 67.0011);
+    //       Placemark place = placemarks[0];
+    //       address.value = 'Address: ${place.street}, ${place.locality}, ${place.administrativeArea}';
+    //      } catch (e) {
+    //      address.value = "Error: $e";
+    //    }
+    // }
+    // Future <void> city(Position position) async {
+    //     try {
+    //       // karachi
+    //       List<Placemark> placemarks = await placemarkFromCoordinates(24.8607, 67.0011);
+    //       Placemark place = placemarks[0];
+    //       cityName.value = 'City: ${place.locality}';
+    //     } catch (e) {
+    //       cityName.value = "Error: $e";
+    //     }
+    //
+    // }
 }
